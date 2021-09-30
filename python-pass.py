ed38991e9f4d075fb51b3655d977949ff208960a
Mohammed falih <mohamedfalih98@gmail.com>
2021-10-01 00:32:27 +0300

    finish the task

--- python-pass.py
+++ python-pass.py
@@ -1,7 +1,14 @@
-
-
 class Solution:
 
     @staticmethod
     def longest_palindromic(s: str) -> str:
-        pass
+         if len(s) <= 1:
+            return s
+        i,k=0,0
+        for j in range(len(s)):
+            if s[j-k: j+1] == s[j-k: j+1][::-1]:
+                i, k = j-k, k+1
+               
+            elif j-k > 0 and s[j-k-1: j+1] == s[j-k-1: j+1][::-1]:
+                i, k = j-k-1, k+2
+        return s[i: i+k]
